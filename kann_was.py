0999eedcd4a7c91a6bc707e451c3511c47f03726
anka009 <Andreas.Kamyschnikow@uks.eu>
2025-11-03 10:36:42 +0100

    Update kann_was.py

--- kann_was.py
+++ kann_was.py
@@ -176,7 +176,7 @@ with col1:
 with col2:
     alpha = st.slider("🌗 Alpha (Kontrast)", 0.1, 3.0, 1.0, step=0.1)
 with col3:
-    circle_radius = st.slider("⚪ Kreisradius (Display-Px)", 1, 20, 5)
+    circle_radius = st.slider("⚪ Kreisradius (Display-Px)", 1, 20, 3)
 
 # -------------------- Modi --------------------
 st.markdown("### 🎨 Modus auswählen")
